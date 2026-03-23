21f8983f9f66b9dee38e3510ac521e4580698648
quocsi014 <quocsi014@gmail.com>
2026-03-23 11:00:17 +0700

    refactor(drive): route identity CQRS through dedicated client

--- modules/drive/adapter/cqrs_bus/index.go
+++ modules/drive/adapter/cqrs_bus/index.go
@@ -5,10 +5,12 @@ import (
 
 	deps "github.com/sky-as-code/nikki-erp/common/deps_inject"
 	identityCqrs "github.com/sky-as-code/nikki-erp/modules/drive/adapter/cqrs_bus/identity_cqrs"
+	identityclient "github.com/sky-as-code/nikki-erp/modules/drive/adapter/cqrs_bus/identity_cqrs/client"
 )
 
 func InitCqrsBusAdaper() error {
 	err := errors.Join(
+		deps.Register(identityclient.NewIdentityCqrsClient),
 		deps.Register(identityCqrs.NewIdentityCqrsAdapter),
 	)
 	return err
