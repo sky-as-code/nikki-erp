304ce1220412e38aebcea650be003601ece48b08
quocsi014 <quocsi014@gmail.com>
2026-04-24 12:04:20 +0700

    fix(crud): fix UpdateBulk commit behavior, and error message for not found

--- modules/core/dynamicmodel/crud/crud_helper.go
+++ modules/core/dynamicmodel/crud/crud_helper.go
@@ -273,7 +273,6 @@ func UpdateBulk[
 	dynamicRepo := param.BaseRepoGetter.GetBaseRepo()
 	allClientErrs := ft.NewClientErrors()
 	allModels := make([]TDomainPtr, 0, len(param.Data))
-	anyNotExisting := false
 
 	for _, dmodelItem := range param.Data {
 		model := TDomainPtr(new(TDomain))
@@ -297,8 +296,7 @@ func UpdateBulk[
 		}
 
 		if !isExisting {
-			anyNotExisting = true
-			continue
+			allClientErrs.Append(*ft.NewAnonymousNotFoundError())
 		}
 
 		allModels = append(allModels, model)
@@ -311,21 +309,20 @@ func UpdateBulk[
 		}, nil
 	}
 
-	if anyNotExisting {
-		return &dyn.OpResult[dyn.MutateResultData]{HasData: false}, nil
-	}
-
 	var totalAffected int
 	var lastAt model.ModelDateTime
 	var lastEtag model.Etag
 
 	var tx database.DbTransaction
+	var isOwnedTx bool
 	if ctx.GetDbTranx() == nil {
 		var err error
 		tx, err = param.BaseRepoGetter.GetBaseRepo().BeginTransaction(ctx)
 		if err != nil {
 			return nil, err
 		}
+
+		isOwnedTx = true
 
 		ctx.SetDbTranx(tx)
 		defer tx.Rollback()
@@ -346,7 +343,7 @@ func UpdateBulk[
 		}
 	}
 
-	if tx != nil {
+	if isOwnedTx {
 		err := tx.Commit()
 		if err != nil {
 			return nil, err
